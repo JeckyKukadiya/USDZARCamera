9784f97e3a7cc4d731807142f3cb4830d0115d2c
Jecky <jackykukadiya@gmail.com>
2024-09-06 15:39:56 +0530

    Initial Commit

---
+++ Sources/USDZARCamera/GuidedCaptureSampleApp.swift
@@ -0,0 +1,13 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+Top-level app structure of the view hierarchy.
+*/
+
+import SwiftUI
+
+struct GuidedCaptureSampleApp{
+    static let subsystem: String = "com.alfianlosari.GuidedCapture"
+}
+ 
